350a92a90153e8c9e3678d02ada6d461a73f659a
Gregor Noczinski <gregor@noczinski.eu>
2019-01-01 17:17:56 +0100

    * Fixed expectation

--- .docker/test-image.sh
+++ .docker/test-image.sh
@@ -9,7 +9,7 @@ export $(cat .docker/build.env | xargs)
 
 set -ex
 
-docker run --rm ${imageName} goxr --version 2>&1          | grep "Version:      TEST${TRAVIS_BRANCH}TEST"
-docker run --rm ${imageName} goxr --version 2>&1          | grep "Git revision: TEST${TRAVIS_COMMIT}TEST"
-docker run --rm ${imageName} goxr-server --version 2>&1   | grep "Version:      TEST${TRAVIS_BRANCH}TEST"
-docker run --rm ${imageName} goxr-server --version 2>&1   | grep "Git revision: TEST${TRAVIS_COMMIT}TEST"
+docker run --rm ${imageName} goxr --version 2>&1          | grep " Version:    TEST${TRAVIS_BRANCH}TEST"
+docker run --rm ${imageName} goxr --version 2>&1          | grep " Revision:   TEST${TRAVIS_COMMIT}TEST"
+docker run --rm ${imageName} goxr-server --version 2>&1   | grep " Version:    TEST${TRAVIS_BRANCH}TEST"
+docker run --rm ${imageName} goxr-server --version 2>&1   | grep " Revision:   TEST${TRAVIS_COMMIT}TEST"
